1e83306bf5be2c885175fa7149cd3baa13316dd4
robertolorenzoaguilar <robertolorenzoaguilarith@gmail.com>
2024-05-15 08:10:33 -0700

    agregando productos impuesots etc

--- POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
+++ POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31e7f463cdd72a3fe36cadddf0bef730fee64313")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efae27e1a5a06df753291cfe96571486dd2d84b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
