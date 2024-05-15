03bd355706c09dde0bb20a38684aaf991c9b3ff6
robertolorenzoaguilar <robertolorenzoaguilarith@gmail.com>
2024-05-14 18:29:37 -0700

    Configurando detalle de venta

--- POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
+++ POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b079aa68737bb9c2487818a5683e02e0b17bb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736fbb7d0926b15a15eae241c7354ce92a4d5df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
