533fce3ae1c2d72800953aee1791a64fb82070f6
robertolorenzoaguilar <robertolorenzoaguilarith@gmail.com>
2024-05-15 17:04:19 -0700

    limpieza

--- POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
+++ POS.Web/obj/Debug/net8.0/POS.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dd922a2b3fc379fae0bce4775d8178d953c6d6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb48dcea3c3f44040467d05e3a999fcd7c1415f")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
